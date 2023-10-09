0714640e2479340bf85b2e187891c477ab44bd66
Soumitra Saha <soumitrosahaofficial@gmail.com>
2023-10-09 19:21:06 +0530

    App File Updated

--- app.js
+++ app.js
@@ -7,10 +7,29 @@ const userRoutes = require('./routes/user.routes.js');
 const postRoutes = require('./routes/post.routes.js');
 const commentRoutes = require('./routes/comment.routes.js');
 const likeRoutes = require('./routes/like.routes.js');
+const projectRoutes = require('./routes/project.routes.js');
+const communityRoutes = require('./routes/community.routes.js');
+const path = require('path');
 
 // Establishing the Database connection
 db.connect();
 const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Public
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(
+  '/uploads/thumbnails',
+  express.static(__dirname + '/uploads/thumbnails')
+);
 
 // Middleware
 app.use(express.json());
@@ -24,5 +43,7 @@ app.use(userRoutes);
 app.use('/posts', postRoutes);
 app.use('/comments', commentRoutes);
 app.use('/likes', likeRoutes);
+app.use('/projects', projectRoutes);
+app.use('/community', communityRoutes);
 
 module.exports = app;
